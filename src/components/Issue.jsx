76fff8ad32b962664f5b31400c882af37d99afb1
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-06-16 00:00:04 -0400

    adding comments.svg

--- src/components/Issue.jsx
+++ src/components/Issue.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css'
 import '../App.css';
 import icon from './icon.svg'
+import commentImg from './comments.svg'
 import { Container } from 'react-bootstrap'
 import moment from 'moment';
 
@@ -16,7 +17,7 @@ function Issue({issue}) {
     <Container className="border">
       {console.log(labels)}
       <ul>
-        <li><div className="main"><a href={html_url} target="_blank">{title}</a> {labels.map(label => <span className="label" style={{backgroundColor: `#${label.color}`}}><a href={label.url} target="_blank">{label.name}</a></span>)}</div><div className="comments"><a className="commentLink" href={comments_url}>{comments ? comments : null}</a></div><br />
+        <li><div className="main"><a href={html_url} target="_blank">{title}</a> {labels.map(label => <span className="label" style={{backgroundColor: `#${label.color}`}}><a href={label.url} target="_blank">{label.name}</a></span>)}</div><div className="comments"><a className="commentLink" href={comments_url}>{comments ? <><img src={commentImg}></img> {comments}</>: null}</a></div><br />
         <span className="number">{`#${number} Opened ${moment(updated_at).fromNow()} hours ago by`} <a className="user" href={user.html_url}>{user.login}</a></span></li>
       </ul>
       {/* <p>{body}</p> */}
