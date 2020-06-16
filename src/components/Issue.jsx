a7ba8003754874567c19aaa7aed6169c127bedb4
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-06-15 23:40:00 -0400

    Working on adding comments section

--- src/components/Issue.jsx
+++ src/components/Issue.jsx
@@ -3,15 +3,21 @@ import 'bootstrap/dist/css/bootstrap.css'
 import '../App.css';
 import icon from './icon.svg'
 import { Container } from 'react-bootstrap'
+import moment from 'moment';
+
 
 function Issue({issue}) {
 
-  const { html_url, title, body, label } = issue;
+  const { html_url, title, body, labels, number, updated_at, user, comments, comments_url } = issue;
+
+  moment().format()
 
   return (
     <Container className="border">
+      {console.log(labels)}
       <ul>
-  <li><a href={html_url} target="_blank">{title}</a>{label}</li>
+        <li><div className="main"><a href={html_url} target="_blank">{title}</a> {labels.map(label => <span className="label" style={{backgroundColor: `#${label.color}`}}><a href={label.url} target="_blank">{label.name}</a></span>)}</div><div className="comments"><a className="commentLink" href={comments_url}>{comments ? comments : null}</a></div><br />
+        <span className="number">{`#${number} Opened ${moment(updated_at).fromNow()} hours ago by`} <a className="user" href={user.html_url}>{user.login}</a></span></li>
       </ul>
       {/* <p>{body}</p> */}
     </Container>
